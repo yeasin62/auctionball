b8fe5240943c3f45c0ca780107adc81ea8de49ff
Gazi Yeasin <gazi.yeasin@yahoo.com>
2026-05-19 21:59:28 +0600

    Fix landing dashboard button label

--- resources/js/Pages/Landing.vue
+++ resources/js/Pages/Landing.vue
@@ -411,7 +411,7 @@ const footerCols = computed(() => [
                             {{ t('auth.verify_log_out') }}
                         </Link>
                         <Link href="/dashboard-redirect" class="btn-primary text-[13px] sm:text-[14px] py-2 px-3 sm:py-2.5 sm:px-4">
-                            {{ t('nav.dashboard') }}
+                            Dashboard
                         </Link>
                     </template>
                     <template v-else>
